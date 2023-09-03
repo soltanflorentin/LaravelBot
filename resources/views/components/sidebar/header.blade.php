c27f848787a4fb5d044fbfd0321b8bc905a5b25a
Florentin <soltanflorentin@gmail.com>
2023-09-03 10:23:49 +0100

    Changed main logo from header

--- resources/views/components/sidebar/header.blade.php
+++ resources/views/components/sidebar/header.blade.php
@@ -1,8 +1,7 @@
 <div class="flex items-center justify-between flex-shrink-0 px-3">
     <!-- Logo -->
     <a href="{{ route('dashboard') }}" class="inline-flex items-center gap-2">
-        <x-application-logo aria-hidden="true" class="w-10 h-auto" />
-        <span class="sr-only">K UI Logo</span>
+        <x-application-logo class="w-20" />
     </a>
 
     <!-- Toggle button -->
