774ea8a7423b9d07f2a32f120b7183c9b903a408
Florentin <soltanflorentin@gmail.com>
2023-09-12 20:36:35 +0100

    Added 1 week cookie for selectable coins

--- app/Http/Livewire/User/Portfolio/Datatable.php
+++ app/Http/Livewire/User/Portfolio/Datatable.php
@@ -6,6 +6,8 @@ use App\Models\User;
 use Livewire\Component;
 use App\Models\Portfolio;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\View\View;
 
 class Datatable extends Component
 {
@@ -19,16 +21,20 @@ class Datatable extends Component
 
     public function mount()
     {
-        //for testing to fakepopulate portofolio table
+        //for testing to fake populate portofolio table
         //Portfolio::createPortfolio();
 
-        $this->newEntryCoin = $this->coins[0];
+        if(!Cookie::get('selectable_coins_symbol_portfolio')) {
+            \Cookie::queue(\Cookie::forever('selectable_coins_symbol_portfolio', $this->getCoinsForCookie(), 10000));
+        }
+
         $this->activeCoins = $this->user->getPortfolios->pluck('symbol')->toArray();
+
+        $this->newEntryCoin = $this->getCoinsProperty()[0];
     }
 
-    public function render()
+    public function render(): View
     {
-        //dd($this->user->getPortfolios);
         return view('livewire.user.portfolio.datatable', [
             'portfolios' => $this->getPortfolio(),
         ]);
@@ -39,27 +45,20 @@ class Datatable extends Component
         return $this->user->getPortfolios;
     }
 
-    public function getCoinsProperty()
-    {
-        $coins = $this->getAllCoins(200)->pluck('symbol')->toArray();
-
-        //dd(array_diff($coins, $this->activeCoins));
-        return array_diff($coins, $this->activeCoins);
-    }
-
-    public function addNewCoin()
+    public function addNewCoin(): void
     {
         Portfolio::create(['symbol' => $this->newEntryCoin, 'user_id' => $this->user->id]);
 
         $this->emit('refreshDatatable');
     }
 
-    public function deleteItem(Portfolio $item)
+    public function deleteItem(Portfolio $item): void
     {
         $item->delete();
 
         $this->emit('refreshDatatable');
     }
+
 
     public function getAllCoins($numberOfCoins)
     {
@@ -68,6 +67,31 @@ class Datatable extends Component
         $allCoins = $coinsRequest->collect();
 
         return $allCoins;
+    }
+
+
+    public function getCoinsProperty(): array
+    {
+        $coins = Cookie::get('selectable_coins_symbol_portfolio');
+        $activeCoins = $this->user->getPortfolios->pluck('symbol')->toArray();
+        //dd(json_decode($coins), $activeCoins);
+
+        return array_diff(json_decode($coins), $activeCoins);
+
+    }
+
+    public function getCoinsForCookie()
+    {
+        $coins = $this->getAllCoins(200)->pluck('symbol')->toArray();
+
+        return json_encode($coins);
+    }
+
+    public function deleteCookie(): void
+    {
+        \Cookie::queue(\Cookie::forget('selectable_coins_symbol_portfolio'));
+
+        $this->emit('refreshDatatable');
     }
 
 
